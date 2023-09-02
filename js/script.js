e03b96351734a5a39b3e0d5a053b9154b209a3bd
Justin Cho Chun Wah <chowah67@gmail.com>
2023-09-02 20:20:35 +0800

    Added Images For index.html and Changes Directory for Css

--- js/script.js
+++ js/script.js
@@ -25,3 +25,17 @@ if (close) {
     navMenu.classList.add("hide");
   });
 }
+
+/*=============== SWIPER ===============*/
+var swiperTrending = new Swiper(".trending__swiper", {
+  spaceBetween: 32,
+  // grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
