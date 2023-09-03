a044fbdd03ae626bbf123ba699654895f72380e5
Justin Cho Chun Wah <chowah67@gmail.com>
2023-09-03 18:30:36 +0800

    Added SVG, Added Newsletter Section, Fixed Some Buges

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/*=============== CHANGE BACKGROUND HEADER =============== */
 function scrollHeader() {
   const header = document.querySelector(".header");
 
@@ -9,7 +9,7 @@ function scrollHeader() {
 
 window.addEventListener("scroll", scrollHeader);
 
-/*=============== HAMBURGER TO CLOSE ICON ===============*/
+/*=============== HAMBURGER TO CLOSE ICON =============== */
 const hamburger = document.querySelector(".fa-bars");
 const close = document.querySelector(".fa-xmark");
 const navMenu = document.querySelector(".nav__menu");
@@ -26,7 +26,7 @@ if (close) {
   });
 }
 
-/*=============== SWIPER TRENDING ===============*/
+/*=============== SWIPER TRENDING =============== */
 var swiperTrending = new Swiper(".trending__swiper", {
   spaceBetween: 32,
   centeredSlides: true,
@@ -39,7 +39,7 @@ var swiperTrending = new Swiper(".trending__swiper", {
   },
 });
 
-/*=============== SWIPER TESTIMONIALS ===============*/
+/*=============== SWIPER TESTIMONIALS =============== */
 var swiperTestimonial = new Swiper(".testimonials__container", {
   spaceBetween: 32,
   grabCursor: true,
@@ -52,3 +52,34 @@ var swiperTestimonial = new Swiper(".testimonials__container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/*=============== NEWSLETTER VALIDATION =============== */
+const newsletterButton = document.getElementById("submit");
+const newsletterName = document.getElementById("name");
+const newsletterEmail = document.getElementById("email");
+const nameError = document.getElementById("name-error");
+const emailError = document.getElementById("email-error");
+
+function validateNewsletter(e) {
+  e.preventDefault();
+
+  // Get the values from the input fields
+  const nameValue = newsletterName.value;
+  const emailValue = newsletterEmail.value;
+
+  // Clear any existing error messages
+  nameError.classList.remove("show");
+  emailError.classList.remove("show");
+
+  if (nameValue === "") {
+    nameError.classList.add("show");
+  } else if (emailValue === "") {
+    emailError.classList.add("show");
+  } else {
+    // Clear the input fields
+    newsletterName.value = "";
+    newsletterEmail.value = "";
+  }
+}
+
+newsletterButton.addEventListener("click", validateNewsletter);
